b74d5a9b5ac13ee0b5961ba78c003b7eae6419a9
Lautaro <lautaro.negri@gmail.com>
2023-03-19 19:17:44 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
+
+# link to the item's search page on MercadoLibre
+link = 'https://listado.mercadolibre.com.ar/playstation-4#D[A:playstation%204]'
+
+def get_prices_by_link(link):
+    # get source
+    r = requests.get(link)
+
+    # parse source
+    page_parse = BeautifulSoup(r.text, 'html.parser')
+
+    # find all list items from search results
+    search_results = page_parse.findAll("li", {"class":"ui-search-layout__item shops__layout-item"})
+
+    # initialise price list
+    item_prices = []
+
+    for result in search_results:
+        price_as_text = result.find("span", {"class":"price-tag-fraction"}).text
+        price = float(price_as_text.replace(".", ""))
+        item_prices.append(price)
+
+    return(item_prices)
+
+def get_average(prices):
+    return np.mean(prices)
+
+print("Price list:",get_prices_by_link(link))
+print("Number of items accounted for:",len(get_prices_by_link(link)))
+print("Average price:",get_average(get_prices_by_link(link)))
